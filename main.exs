62bc31eba69d3c58f4035ea377e883862fd5ddf4
poli <katunina0polina@gmail.com>
2023-03-13 12:05:39 +0300

    main.exs: add uptime var

--- main.exs
+++ main.exs
@@ -2,6 +2,9 @@ os_release = File.read!("/etc/os-release")
              |> String.split(["\n", "="], [trim: true])
              |> Enum.chunk_every(2)
              |> Map.new(fn [k, v] -> {k, v} end)
-uptime = File.read!("/proc/uptime") 
+uptime = File.read!("/proc/uptime")
+         |> String.split(" ")
+         |> hd 
+         |> String.to_float
 shell = System.get_env("SHELL")
 version = File.read!("/proc/version") 
