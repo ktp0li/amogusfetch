fbe9d2d3229a1e652c338bc8679c2aae5c3d96be
poli <katunina0polina@gmail.com>
2023-03-14 14:02:51 +0300

    main.exs: add hostname and user var, code formatting

--- main.exs
+++ main.exs
@@ -1,21 +1,38 @@
-os_release = File.read!("/etc/os-release")
-             |> String.split(["\n", "="], trim: true)
-             |> Enum.chunk_every(2)
-             |> Map.new(fn [k, v] -> {k, v} end)
-uptime = File.read!("/proc/uptime")
-         |> String.split(" ")
-         |> hd 
-         |> String.to_float
-         |> round
-         |> then(fn sec -> Time.add(~T[00:00:00], sec, :second) end)
+os_release =
+  File.read!("/etc/os-release")
+  |> String.split(["\n", "="], trim: true)
+  |> Enum.chunk_every(2)
+  |> Map.new(fn [k, v] -> {k, v} end)
+
+uptime =
+  File.read!("/proc/uptime")
+  |> String.split(" ")
+  |> hd
+  |> String.to_float()
+  |> round
+  |> then(fn sec -> Time.add(~T[00:00:00], sec, :second) end)
+
 shell = System.get_env("SHELL")
-version = File.read!("/proc/sys/kernel/osrelease")
-          |> String.trim
-mem = File.read!("/proc/meminfo")
-      |> then(fn x ->
-      Regex.named_captures(~r/^MemTotal:\s*(?<all>\d+)\X*^MemFree:\s*(?<free>\d+)/m, x) end)
-      |> Map.new(fn {k, v} -> {k, String.to_integer(v) |> div(1024)} end)
-cpu = File.read!("/proc/cpuinfo")
-      |> then(fn x -> Regex.run(~r/^model name\W*(\N+)/m, x, capture: :all_but_first) end)
-      |> List.to_string
-      |> String.downcase
+
+version =
+  File.read!("/proc/sys/kernel/osrelease")
+  |> String.trim()
+
+mem =
+  File.read!("/proc/meminfo")
+  |> then(fn x ->
+    Regex.named_captures(~r/^MemTotal:\s*(?<all>\d+)\X*^MemFree:\s*(?<free>\d+)/m, x)
+  end)
+  |> Map.new(fn {k, v} -> {k, String.to_integer(v) |> div(1024)} end)
+
+cpu =
+  File.read!("/proc/cpuinfo")
+  |> then(fn x -> Regex.run(~r/^model name\W*(\N+)/m, x, capture: :all_but_first) end)
+  |> List.to_string()
+  |> String.downcase()
+
+user = System.get_env("USER")
+
+hostname =
+  File.read!("/etc/hostname")
+  |> String.trim()
